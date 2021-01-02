3cf109e0b720739ef19627556e9e711bd6dda888
Duminda <duminda@example.com>
2021-01-02 11:31:44 +0530

    Add two more projects called AsyncVsThreading & AsyncVsThreadWindowsForm to the solution

---
+++ AsyncVsThreadWindowsForms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncVsThreadWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            SomeMethod();
+        }
+
+        public async void SomeMethod()
+        {
+            await Task.Delay(20000);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ AsyncVsThreading/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncVsThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code 1");
+            Console.WriteLine("Code 2 " + Thread.CurrentThread.ManagedThreadId);
+            SomeMethoD();
+            Console.WriteLine("Code 7");
+            Console.WriteLine("Code 8");
+            Console.Read();
+
+        }
+
+        static async void SomeMethoD()
+        {
+            Console.WriteLine("Code 3");
+            Console.WriteLine("Code 4");
+
+            await Task.Delay(20000); // 20 seconds of delay
+            Console.WriteLine("Code 5");
+            Console.WriteLine("Code 6 " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        /*
+        static void Main(string[] args)
+        {
+            Thread x = new Thread(SomeMethoD);
+            x.Start();
+            //SomeMethoD();
+            Console.WriteLine("Main method code");
+            Console.Read();
+        }
+
+        static void SomeMethoD()
+        {
+            Task.Delay(60000).Wait();
+            Console.WriteLine("Async code finishes");
+        }
+
+        static async void SomeMethoD()
+        {
+            await Task.Delay(60000); // 60 seconds of delay
+            Console.WriteLine("Async code finishes");
+        }
+        */
+    }
+}
